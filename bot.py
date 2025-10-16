e2d262b9ae1e84fe70f04aa027abf567f9c42b7e
Gregory Melikhov <gr.melikhov12@gmail.com>
2025-10-16 19:34:34 +0300

    Update bot.py with new handler.

--- bot.py
+++ bot.py
@@ -22,5 +22,19 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        
+@bot.message_handler(commands=['unban'])
+def unban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        bot.unban_chat_member(chat_id, user_id) # пользователь с user_id будет разбанен в чате с chat_id
+        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был разбанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите разбанить.")
+
 
 bot.infinity_polling(none_stop=True)
+
